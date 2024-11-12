cd211130b0e0a9c684ab1e8f09e21199a8d9e9ed
preeti04barfa <preetibarfa.vhits@gmail.com>
2024-11-12 19:50:30 +0530

    working on header

--- frontend/my-app/src/component/Home.js
+++ frontend/my-app/src/component/Home.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
-import "./Home.css"
+import "./Home.css";
+import Header from './Header';
+
 
 const Home = () => {
   return (
     <>
+      <Box><Header/></Box>
     <Box className="main-sidebar">
     <Box>
       <Sidebar/>
